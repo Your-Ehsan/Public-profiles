e2fada42401aff1e910c45124fa57a29fb3e3161
your-ehsan <ehsanshahid787@gmail.com>
2023-09-01 00:49:01 +0500

    initial changes

--- src/lib/firebase/init.ts
+++ src/lib/firebase/init.ts
@@ -48,11 +48,11 @@ const InitServices = (configs: FirebaseOptions) => {
     firestore: Firestore;
     storage: FirebaseStorage;
   }) => {
-    if (
-      process.env.NODE_ENV !== "production"
-    ) {
+    if (process.env.NODE_ENV !== "production") {
       try {
-        connectAuthEmulator(auth, "http://localhost:9099");
+        connectAuthEmulator(auth, "http://localhost:9099", {
+          disableWarnings: false,
+        });
         connectFirestoreEmulator(firestore, "localhost", 8080);
         connectStorageEmulator(storage, "localhost", 9199);
       } catch (error) {
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 
-module.exports = {
+import { Config } from "tailwindcss";
+
+const configs: Config = {
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -59,12 +61,12 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: 0 },
+          from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: 0 },
+          to: { height: "0" },
         },
       },
       animation: {
@@ -75,3 +77,5 @@ module.exports = {
   },
   plugins: [require("tailwindcss-animate")],
 };
+
+module.exports = configs;
